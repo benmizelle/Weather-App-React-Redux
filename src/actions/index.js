bc25c686560e05b3bbe5d12d9e494752c6e8c5c8
H Cindymizelle M <cindymizelle@iMac.local>
2017-07-05 21:49:12 -0400

    functioning

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '2015ff6addc3be0c292a75f6c08fd90a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
